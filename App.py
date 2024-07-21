6ebd375732bbb1e7df8e00cfa5248ca264f89350
grantmorton1965 <grantm685@gmail.com>
2024-07-21 12:50:29 -0500

    Update App.py

--- App.py
+++ App.py
@@ -198,7 +198,7 @@ def save_as_pdf(cartons_df, item_data, best_fit_container, best_fit_volume_utili
         c.save()
         return tmpfile.name
 
-def pack_items(carton, item_data, batch_size=300, num_batches=2):
+def pack_items(carton, item_data, batch_size=100, num_batches=6):
     storage_unit = Bin(carton['Description'], carton['ID Length (in)'], carton['ID Width (in)'], carton['ID Height (in)'], 1)
     packer = Packer()
     packer.add_bin(storage_unit)
